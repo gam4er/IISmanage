e556b7d5bc1d369d7de851238a6eb41f86a6c127
Alex Rodchenko <gam4er@gmail.com>
2023-05-29 18:51:25 +0300

    list vdir

--- IISmanage/Program.cs
+++ IISmanage/Program.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Web.Administration;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IISmanage
 {
@@ -12,25 +8,44 @@ namespace IISmanage
         static void Main(string[] args)
         {
             ServerManager serverManager = new ServerManager();
-            Configuration config = serverManager.GetApplicationHostConfiguration();
 
-            ConfigurationSection applicationPoolsSection = config.GetSection("system.applicationHost/applicationPools");
+            SiteCollection sites = serverManager.Sites;
+            foreach (Site site in sites)
+            {
+                ConfigurationElementCollection siteCollection = site.GetCollection();
+                foreach (ConfigurationElement element in siteCollection)
+                {
+                    ConfigurationElementCollection virtualDirectoryElement = element.GetCollection();
+                    foreach (ConfigurationElement element2 in virtualDirectoryElement)
+                    {
+                        var PPath  = element2.GetAttribute("physicalPath");
+                        var path = element2.GetAttribute("path");
+                        Console.WriteLine("Root Vdir {0} have phPath {1}", path.Value, PPath.Value);                        
+                    }
+                }
+            }
 
-            ConfigurationElementCollection applicationPoolsCollection = applicationPoolsSection.GetCollection();
+            Console.ReadLine();
+
+            //Configuration config = serverManager.GetApplicationHostConfiguration();
+
+            //ConfigurationSection applicationPoolsSection = config.GetSection("system.applicationHost/applicationPools");
+
+            //ConfigurationElementCollection applicationPoolsCollection = applicationPoolsSection.GetCollection();
 
             //ConfigurationElement addElement = applicationPoolsCollection.CreateElement("add");
             //addElement["name"] = @"applicationPool1";
 
-            ConfigurationElement addElement = applicationPoolsCollection.GetCollection().SingleOrDefault(n => n.GetAttributeValue("name").ToString() == ".NET v4.5 Classic");
+            //ConfigurationElement addElement = applicationPoolsCollection.GetCollection().SingleOrDefault(n => n.GetAttributeValue("name").ToString() == ".NET v4.5 Classic");
             //addElement["name"] = @"applicationPool1";
 
-            ConfigurationElement processModelElement = addElement.ChildElements["processModel"];
-            processModelElement["identityType"] = @"LocalSystem";
-            processModelElement["userName"] = @"";
-            processModelElement["password"] = @"";
+            //ConfigurationElement processModelElement = addElement.ChildElements["processModel"];
+            //processModelElement["identityType"] = @"LocalSystem";
+            //processModelElement["userName"] = @"";
+            //processModelElement["password"] = @"";
             //applicationPoolsCollection.Add(addElement);
 
-            serverManager.CommitChanges();
+            //serverManager.CommitChanges();
         }
     }
 }
